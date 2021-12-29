d9a7979c0b7e48b8f90ce79e24ad64bc83857401
LiorRavid <simoplay@gmail.com>
2021-12-29 19:13:07 +0200

    added img+video

---
+++ js/apps/keep/cmps/NoteList.jsx
@@ -0,0 +1,12 @@
+
+import { NotePreview } from './NotePreview.jsx'
+
+export function NoteList({ notes }) {
+    return (
+        <section className="note-list">
+            <ul className="note-list-ul clean-list ">
+                    {notes.map(note => <li className="note-preview-container" key={note.id}><NotePreview note={note}/></li>)}
+            </ul>
+        </section>
+    )
+}
---
+++ js/apps/keep/cmps/note-type/DynamicCmp.jsx
@@ -0,0 +1,21 @@
+import { NoteImg } from './NoteImg.jsx'
+import { NoteTodos} from './NoteTodos'
+import { NoteTxt } from './NoteTxt.jsx'
+import { NoteVideo} from './NoteVideo.jsx'
+
+
+
+export function DynamicCmp(props) {
+    switch (props.type) {
+        case 'note-img':
+            return <NoteImg {...props} />
+        case 'note-todos':
+            return <NoteTodos {...props} />
+        case 'note-txt':
+            return < NoteTxt {...props} />
+        case 'note-video':
+            return <NoteVideo {...props} />
+        default:
+            return <React.Fragment></React.Fragment>
+    }
+}
