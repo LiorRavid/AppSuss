399174b4072aea78d61f83d985475451473fa8bb
LiorRavid <simoplay@gmail.com>
2021-12-29 13:20:12 +0200

    App draft2

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import { AppHome } from './js/pages/AppHome.jsx'
 // import { AppAbout } from './js/pages/AppAbout.jsx'
-// import { MailApp } from './js/pages/MailApp.jsx'
+import { MailApp } from './js/pages/MailApp.jsx'
 // import { MailDetails } from './pages/MailDetails.jsx'
 // import { MailEdit } from './pages/MailEdit.jsx'
-// import { KeepApp } from './js/pages/KeepApp.jsx'
+import { KeepApp } from './js/pages/KeepApp.jsx'
 // import { NoteDetails } from './pages/NoteDetails.jsx'
 // import { NoteEdit } from './pages/NoteEdit.jsx'
 
@@ -25,12 +25,12 @@ export function App() {
                 <main className="app-main-layout flex-grow">
                     <Switch>
                         {/* <Route component={NoteEdit} path="keep/note/edit/:noteId?" />
-                        <Route component={NoteDetails} path="/keep/note/:noteId" />
+                        <Route component={NoteDetails} path="/keep/note/:noteId" /> */}
                         <Route component={KeepApp} path="/keep" />
-                        <Route component={MailEdit} path="/mail/edit/:mailId?" />
-                        <Route component={MailDetails} path="/mail/:mailId" />
+                        {/* <Route component={MailEdit} path="/mail/edit/:mailId?" />
+                        <Route component={MailDetails} path="/mail/:mailId" /> */}
                         <Route component={MailApp} path="/mail" />
-                        <Route component={AppAbout} path="/about" /> */}
+                        {/* <Route component={AppAbout} path="/about" /> */}
                         <Route component={AppHome} path="/" />
                     </Switch>
                 </main>
